edb44237f94575397abb3cc704fa75a85fa38a3b
Cameron McNiff <112502675+CMcN-38@users.noreply.github.com>
2024-02-27 21:52:54 +0000

    More NixOS tweaks

--- install.sh
+++ install.sh
@@ -31,6 +31,7 @@ ln -s ~/.dotfiles/lazygit ~/.config/lazygit
 ln -s ~/.dotfiles/mpv ~/.config/mpv
 ln -s ~/.dotfiles/nvim ~/.config/nvim
 ln -s ~/.dotfiles/starship ~/.config/starship
+ln -s ~/.dotfiles/swappy ~/.config/swappy
 ln -s ~/.dotfiles/swaync ~/.config/swaync
 ln -s ~/.dotfiles/tmux ~/.config/tmux
 ln -s ~/.dotfiles/waybar ~/.config/waybar
@@ -44,6 +45,8 @@ ln -s ~/.dotfiles/.tmux ~/.tmux
 sudo ln -s ~/.dotfiles/scripts/random_wallpaper /usr/local/bin/random_wallpaper
 sudo ln -s ~/.dotfiles/scripts/tmux-sessioniser /usr/local/bin/tmux-sessioniser
 sudo ln -s ~/.dotfiles/scripts/tmux-cht.sh /usr/local/bin/tmux-cht.sh
+sudo ln -s ~/.dotfiles/scripts/rebuild.sh /usr/local/bin/rebuild
+sudo ln -s ~/.dotfiles/scripts/screenshot.sh /usr/local/bin/screenshot
 
 sudo ln -s ~/.config/zsh/.zshrc ~/.zshrc
 #  ___  __  ____    ____  ____  ____  _  _  ____ 
---
+++ scripts/rebuild.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -e
+pushd /home/cameron/.nixos/
+sudo nvim configuration.nix
+alejandra . &>/dev/null
+git diff -U0 *.nix
+echo "NixOS Rebuilding..."
+# The following line is the rebuild command, this would need altering along with new hosts
+sudo nixos-rebuild switch --flake /home/cameron/.nixos/#targaryen &> nixos-switch.log || (
+	sudo cat nixos-switch.log | grep --color error && false)
+#gen=$(nixos-rebuild list-generations | grep current)
+git commit -am "$(date)"
+popd
+notify-send -e "NixOS Rebuilt OK!" --icon=software-update-available
+
